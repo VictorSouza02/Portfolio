5419e3e7e2708764a62f5d7b7c47d2901cf0b9ff
Souza Victor <victorbatistasouza2002@gmail.com>
2023-01-04 18:44:02 -0300

    about texts finished and componentized

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,4 @@
+import AboutText from '../components/about/aboutText';
 import aboutPhoto from '../media/about-picture.jpg'
 
 const About = () => {
@@ -5,17 +6,9 @@ const About = () => {
         <section>
             <h1 className="text-4xl font-black md:self-start">About</h1>
 
-            <p className="text-xl">I'm Víctor, a web developer focused on the client side, in other words a <span className='text-3xl font-black text-[#bf9f4c]'>front end developer</span>.</p>
+            <AboutText/>
 
-            <p className="text-xl">My life as a programmer started in 2019 when I heard about Python. Since then I have fallen more and more in love with this area. I've been doing projects to help with my study, some too ambitious, but I believe that the more difficult the greater the learning.</p>
-
-            <div className='flex gap-8 justify-between'>
-                <img className='w-[200px] max-md:hidden' src={aboutPhoto} alt="Víctor Photo" />
-
-                <p className="text-xl">when I started studying CSS I was completely fascinated, out of curiosity I tried to do things I hadn't even learned yet, one of these examples was my clone of the steam layout. This layout was made completely without using Display: flex or Grid, only with margins, paddings.</p>
-            </div>
-
-            <h1 className="text-2xl font-black md:self-start">Work experience</h1>
+            <h2 className="text-2xl font-black md:self-start">Work experience</h2>
         </section>
     )
 }
