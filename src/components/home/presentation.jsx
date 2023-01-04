c19eaca8b2df5f761f3f69c41fd3bde56ee91be5
Souza Victor <victorbatistasouza2002@gmail.com>
2023-01-04 13:26:45 -0300

    background added

--- src/components/home/presentation.jsx
+++ src/components/home/presentation.jsx
@@ -6,7 +6,7 @@ const Presentation = () => {
             <div className="w-full flex flex-col items-center justify-center md:flex-row md:justify-between gap-4">
                 <img className="md:order-2 rounded-full max-w-[175px]" src="https://media.licdn.com/dms/image/D4D03AQFKLHUKHJq17A/profile-displayphoto-shrink_200_200/0/1664487349109?e=1678320000&v=beta&t=14K8u1ILdSP7mwsTKdGb1J1eJk8TRRf0H43K7FfQPx8" alt="Victor photo" />
 
-                <p className="text-4xl sm:text-5xl text-center md:text-start max-w-[625px] w-full">I'm <span className="text-[#5c3e8d] font-black">Víctor</span>. I'm a Front-end developer.</p>
+                <p className="text-4xl sm:text-5xl text-center md:text-start max-w-[625px] w-full">I'm <span className="text-[#5b7493] font-black">Víctor</span>. I'm a Front-end developer.</p>
             </div>
 
             <div className="md:order-2 flex flex-col gap-4 md:flex-row md:self-start">
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,14 +1,14 @@
 const Projects = () => {
     return (
-        <section className="w-full flex flex-col items-center gap-8 max-w-[900px]">
-            <h1 className="text-4xl md:self-start">Projects</h1>
+            <section>
+                <h1 className="text-4xl md:self-start">Projects</h1>
 
-            <div className="grid grid-cols-1 gap-8 md:gap-6 md:grid-cols-3 lg:grid-cols-4 w-full">
-                <div className="w-[100%] aspect-[1/1.3] shadow-lg rounded-2xl">
+                <div className="grid grid-cols-1 gap-8 md:gap-6 md:grid-cols-3 lg:grid-cols-4 w-full">
+                    <div className="w-[100%] aspect-[1/1.3] shadow-lg rounded-2xl">
 
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
     )
 }
 
