b05ef6579c6347c2233e1e13bd14d8090f9a0afd
Souza Victor <victorbatistasouza2002@gmail.com>
2023-01-04 17:18:01 -0300

    about photo solved responsivity

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <p className="text-xl">My life as a programmer started in 2019 when I heard about Python. Since then I have fallen more and more in love with this area. I've been doing projects to help with my study, some too ambitious, but I believe that the more difficult the greater the learning.</p>
 
             <div className='flex gap-8 justify-between'>
-                <img className='w-[200px] max-sm:hidden' src={aboutPhoto} alt="Víctor Photo" />
+                <img className='w-[200px] max-md:hidden' src={aboutPhoto} alt="Víctor Photo" />
 
                 <p className="text-xl">when I started studying CSS I was completely fascinated, out of curiosity I tried to do things I hadn't even learned yet, one of these examples was my clone of the steam layout. This layout was made completely without using Display: flex or Grid, only with margins, paddings.</p>
             </div>
