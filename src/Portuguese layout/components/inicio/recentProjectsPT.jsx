3688b88c504915b84c3ed0d9dd6b57899275530e
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-01-07 09:33:13 -0300

    RecentProjectsPT href removed by return to EN

--- src/Portuguese layout/components/inicio/recentProjectsPT.jsx
+++ src/Portuguese layout/components/inicio/recentProjectsPT.jsx
@@ -6,35 +6,35 @@ const recentProjectsPT = () => {
             <h2 className="text-3xl text-center sm:text-start md:self-start">Projetos Recentes</h2>
 
             <div className="grid grid-cols-1 gap-8 md:gap-6 md:grid-cols-3 w-full">
-                <Link
-                   to="/pt/"
+                <a
+                   
                     id='project-coming'
                     className="bg-white project-card-container w-[100%] aspect-[1/1.3] shadow-lg rounded-2xl flex flex-col justify-end overflow-hidden">
                     <div className="order-2 project-description w-full bg-[rgba(177,177,177,0.6)] p-2 flex-col items-center md:items-start">
                         <h3 className='text-base font-black'>Em breve</h3>
                         <span className='text-sm text-[#111827cc]'>xx/xx/xx</span>
                     </div>
-                </Link>
+                </a>
 
-                <Link
-                   to="/pt/"
+                <a
+                   
                     id='project-coming'
                     className="bg-white project-card-container w-[100%] aspect-[1/1.3] shadow-lg rounded-2xl flex flex-col justify-end overflow-hidden">
                     <div className="order-2 project-description w-full bg-[rgba(177,177,177,0.6)] p-2 flex-col items-center md:items-start">
                         <h3 className='text-base font-black'>Em breve</h3>
                         <span className='text-sm text-[#111827cc]'>xx/xx/xx</span>
                     </div>
-                </Link>
+                </a>
 
-                <Link
-                   to="/pt/"
+                <a
+                   
                     id='project-coming'
                     className="bg-white project-card-container w-[100%] aspect-[1/1.3] shadow-lg rounded-2xl flex flex-col justify-end overflow-hidden">
                     <div className="order-2 project-description w-full bg-[rgba(177,177,177,0.6)] p-2 flex-col items-center md:items-start">
                         <h3 className='text-base font-black'>Em breve</h3>
                         <span className='text-sm text-[#111827cc]'>xx/xx/xx</span>
                     </div>
-                </Link>
+                </a>
             </div>
 
             <Link
