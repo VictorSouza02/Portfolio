4d1bc1558fbe1d664191c33ffac60b29a0ceb832
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-01-07 09:45:21 -0300

    aboutTextPT finished and index no translate

--- src/Portuguese layout/components/sobre/aboutTextPT.jsx
+++ src/Portuguese layout/components/sobre/aboutTextPT.jsx
@@ -6,33 +6,37 @@ const AboutText = () => {
             <h1 className="text-4xl font-black md:self-start">Sobre</h1>
 
             <p className="text-xl">
-                I'm Víctor, a web developer focused on the client side, in other words a
-                <span className='text-3xl font-black text-[#bf9f4c]'> front end developer</span>.
+                Eu sou Víctor, um desenvolvedor web focado no client side, em outras palavras um
+                <span className='text-3xl font-black text-[#bf9f4c]'> desenvolvedor front end</span>.
             </p>
 
-            <p className="text-xl">My life as a programmer started in 2019 when I heard about Python.
-                Since then I have fallen more and more in love with this area. I've been doing projects to help with my study,
-                some too ambitious,
-                but I believe that the more difficult the greater the learning.
+            <p className="text-xl">Minha vida como programador começou em 2019 quando ouvi falar sobre Python
+                . Desde então tenho me apaixonado cada vez mais por essa área. Tenho feito projetos para ajudar no meu
+                estudo, alguns ambiciosos demais, mas acredito que quanto mais difícil maior o aprendizado.
             </p>
 
             <div className='flex flex-row justify-between gap-8 flex-wrap w-full'>
                 <img className="w-[40%] object-contain hidden md:block" src={aboutPhoto} alt="Víctor" />
 
                 <div className='flex flex-col gap-8 md:w-[53%]'>
-                    <p className="text-xl">when I started studying CSS I was completely fascinated, out of curiosity I
-                        tried to do things I hadn't even learned yet, one of these examples was my clone of the
-                        <span> <a target="_blank" rel="noreferrer" href="https://victorsouza02.github.io/Steam-Layout/" className='about-underline'>steam layout</a></span>
-                        . This layout was made completely without using Display: flex or Grid, only with margins, paddings, what made it a non-responsive page.
+                    <p className="text-xl">quando comecei a estudar CSS fiquei completamente fascinado, por
+                        curiosidade tentei fazer coisas que ainda nem havia aprendido, um desses exemplos foi meu clone do
+                        <span> <a target="_blank" rel="noreferrer"
+                            href="https://victorsouza02.github.io/Steam-Layout/"
+                            className='about-underline'>Layout Steam
+                        </a>
+                        </span>
+                        . Este layout foi feito totalmente sem usar Display: flex ou Grid, apenas com margins, paddings, 
+                        tornando o projeto nada responsivo.
                     </p>
                 </div>
 
-                <p className="text-xl">Can you find me on
+                <p className="text-xl">Você pode me encontar no
                     <span> <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/victorsouza02" className='about-underline'>LinkedIn</a></span>
-                    , or you can follow me on
+                    , ou pode me seguir no
                     <span> <a target="_blank" rel="noreferrer" href="https://www.instagram.com/victorsouza.png/" className='about-underline'>
                         Instagram</a>
-                    </span> where I share a little about my personal life. If you want to see my projects in more detail, follow me on
+                    </span> onde eu publico sobre minha vida pessoal. Se você deseja ver meus projetos com mais detalhes, me siga no
                     <span> <a target="_blank" rel="noreferrer" href="https://github.com/VictorSouza02" className='about-underline'>GitHub</a></span>!
                 </p>
             </div>
