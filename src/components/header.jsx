54cbde3a0832c9a17614923f488c00ee46c9a53d
Souza Victor <victorbatistasouza2002@gmail.com>
2023-01-06 22:25:41 -0300

    Indent Header fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,26 +13,61 @@ const Header = () => {
         <header className="w-full">
             <nav className="relative w-full flex py-4 navbar navbar-expand-md">
                 <div className="container-fluid w-full flex flex-wrap items-center justify-end">
-                    <button className="navbar-toggler justify-self-start text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline " type="button" data-bs-toggle="collapse" data-bs-target="#headerNavBar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <img className="w-6" src="https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/three-horizontal-lines-outline-icon.svg" alt="Menu button" />
+                    <button className="navbar-toggler justify-self-start text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 
+                    px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline "
+                        type="button"
+                        data-bs-toggle="collapse" data-bs-target="#headerNavBar" aria-controls="navbarSupportedContent" 
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <img className="w-6"
+                            src="https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/three-horizontal-lines-outline-icon.svg"
+                            alt="Menu button" />
                     </button>
 
-                    <div className="collapse navbar-collapse flex-grow items-center bg-gray-100 max-md:absolute max-md:top-12 max-md:left-[50%] max-md:translate-x-[-50%] w-[60%] p-2 md:bg-inherit shadow-lg md:shadow-inherit rounded-xl" id="headerNavBar">
-                        <ul className="navbar-nav flex flex-col list-style-none w-full justify-center items-center gap-2 lg:gap-10 text-[#111827b3] text-lg font-bold">
+                    <div
+                        className="collapse navbar-collapse flex-grow items-center bg-gray-100 max-md:absolute max-md:top-12 max-md:left-[50%] 
+                    max-md:translate-x-[-50%] w-[60%] p-2 md:bg-inherit shadow-lg md:shadow-inherit rounded-xl"
+                        id="headerNavBar">
+                        <ul
+                            className="navbar-nav flex flex-col list-style-none w-full justify-center items-center gap-2 
+                        lg:gap-10 text-[#111827b3] text-lg font-bold">
                             <li className="nav-item p-2">
-                                <Link className={`${homeRoute === pathname ? 'text-[#111827]' : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`} to="/">Home</Link>
+                                <Link
+                                    className={`${homeRoute === pathname
+                                        ? 'text-[#111827]'
+                                        : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`}
+                                    to="/">
+                                    Home
+                                </Link>
                             </li>
 
                             <li className="nav-item p-2">
-                                <Link className={`${aboutRoute === pathname ? 'text-[#111827]' : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`} to="/about">About</Link>
+                                <Link
+                                    className={`${aboutRoute === pathname
+                                        ? 'text-[#111827]'
+                                        : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`}
+                                    to="/about">
+                                    About
+                                </Link>
                             </li>
 
                             <li className="nav-item p-2">
-                                <Link className={`${projectsRoute === pathname ? 'text-[#111827]' : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`} to="/projects">Projects</Link>
+                                <Link
+                                    className={`${projectsRoute === pathname
+                                        ? 'text-[#111827]'
+                                        : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`}
+                                    to="/projects">
+                                    Projects
+                                </Link>
                             </li>
 
                             <li className="nav-item p-2">
-                                <Link className={`${contactRoute === pathname ? 'text-[#111827]' : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`} to="/contact">Contact</Link>
+                                <Link
+                                    className={`${contactRoute === pathname
+                                        ? 'text-[#111827]'
+                                        : 'hover-underline-animation hover:drop-shadow-lg hover:text-[#111827]'}`}
+                                    to="/contact">
+                                    Contact
+                                </Link>
                             </li>
                         </ul>
                     </div>
