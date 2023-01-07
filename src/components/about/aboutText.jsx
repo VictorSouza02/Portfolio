407197648a24be121a16b30bc229e4dd7793b46e
Souza Victor <victorbatistasouza2002@gmail.com>
2023-01-06 21:54:29 -0300

    fixed ident and home text

--- src/components/about/aboutText.jsx
+++ src/components/about/aboutText.jsx
@@ -5,12 +5,15 @@ const AboutText = () => {
         <>
             <h1 className="text-4xl font-black md:self-start">About</h1>
 
-            <p className="text-xl">I'm Víctor, a web developer focused on the client side, in other words a
-                <span className='text-3xl font-black text-[#bf9f4c]'>front end developer</span>.</p>
+            <p className="text-xl">
+                I'm Víctor, a web developer focused on the client side, in other words a
+                <span className='text-3xl font-black text-[#bf9f4c]'> front end developer</span>.
+            </p>
 
             <p className="text-xl">My life as a programmer started in 2019 when I heard about Python.
                 Since then I have fallen more and more in love with this area. I've been doing projects to help with my study, some too ambitious,
-                but I believe that the more difficult the greater the learning.</p>
+                but I believe that the more difficult the greater the learning.
+            </p>
 
             <div className='flex flex-row justify-between gap-8 flex-wrap w-full'>
                 <img className="w-[40%] object-contain hidden md:block" src={aboutPhoto} alt="Víctor" />
@@ -18,15 +21,19 @@ const AboutText = () => {
                 <div className='flex flex-col gap-8 md:w-[53%]'>
                     <p className="text-xl">when I started studying CSS I was completely fascinated, out of curiosity I
                         tried to do things I hadn't even learned yet, one of these examples was my clone of the
-                        <span><a target="_blank" rel="noreferrer" href="https://victorsouza02.github.io/Steam-Layout/" className='about-underline'>steam layout</a></span>
-                        . This layout was made completely without using Display: flex or Grid, only with margins, paddings, what made it a non-responsive page.</p>
+                        <span> <a target="_blank" rel="noreferrer" href="https://victorsouza02.github.io/Steam-Layout/" className='about-underline'>steam layout</a></span>
+                        . This layout was made completely without using Display: flex or Grid, only with margins, paddings, what made it a non-responsive page.
+                    </p>
                 </div>
 
                 <p className="text-xl">Can you find me on
-                    <span><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/victorsouza02" className='about-underline'>LinkedIn</a></span>
-                    , or you can follow me on <span><a target="_blank" rel="noreferrer" href="https://www.instagram.com/victorsouza.png/" className='about-underline'>
-                        Instagram</a></span> where I share a little about my personal life. If you want to see my projects in more detail, follow me on
-                    <span><a target="_blank" rel="noreferrer" href="https://github.com/VictorSouza02" className='about-underline'>GitHub</a></span>!</p>
+                    <span> <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/victorsouza02" className='about-underline'>LinkedIn</a></span>
+                    , or you can follow me on
+                    <span> <a target="_blank" rel="noreferrer" href="https://www.instagram.com/victorsouza.png/" className='about-underline'>
+                        Instagram</a>
+                    </span> where I share a little about my personal life. If you want to see my projects in more detail, follow me on
+                    <span> <a target="_blank" rel="noreferrer" href="https://github.com/VictorSouza02" className='about-underline'>GitHub</a></span>!
+                </p>
             </div>
         </>
     )
--- src/components/home/presentation.jsx
+++ src/components/home/presentation.jsx
@@ -4,15 +4,33 @@ const Presentation = () => {
     return (
         <section>
             <div className="w-full flex flex-col items-center justify-center md:flex-row md:justify-between gap-4">
-                <img className="md:order-2 rounded-full max-w-[175px]" src="https://media.licdn.com/dms/image/D4D03AQFKLHUKHJq17A/profile-displayphoto-shrink_200_200/0/1664487349109?e=1678320000&v=beta&t=14K8u1ILdSP7mwsTKdGb1J1eJk8TRRf0H43K7FfQPx8" alt="Victor" />
+                <img
+                    className="md:order-2 rounded-full max-w-[175px]"
+                    src="https://media.licdn.com/dms/image/D4D03AQFKLHUKHJq17A/profile-displayphoto-shrink_200_200/0/1664487349109?e=1678320000&v=beta&t=14K8u1ILdSP7mwsTKdGb1J1eJk8TRRf0H43K7FfQPx8"
+                    alt="Victor"
+                />
 
-                <p className="text-4xl sm:text-5xl text-center md:text-start max-w-[625px] w-full">I'm <span className="text-[#5b7493] font-black">teste</span>. I'm a Front-end developer.</p>
+                <p className="text-4xl sm:text-5xl text-center md:text-start max-w-[625px] w-full">
+                    I'm
+                    <span className="text-[#5b7493] font-black"> Víctor</span>
+                    . I'm a Front-end developer.
+                </p>
             </div>
 
             <div className="md:order-2 flex flex-col gap-4 md:flex-row md:self-start">
-                <Link className="py-1 px-2 bg-[#5b7493] text-white button-transition hover:bg-inherit hover:text-[#5b7493] border hover:border-[#5b7493] rounded-2xl text-center" to="/about">More about me</Link>
+                <Link
+                    className="py-1 px-2 bg-[#5b7493] text-white button-transition hover:bg-inherit 
+                hover:text-[#5b7493] border hover:border-[#5b7493] rounded-2xl text-center"
+                    to="/about">
+                    More about me
+                </Link>
 
-                <Link className="py-1 px-2 border border-[#5b7493] text-[#5b7493] button-transition hover:bg-[#5b7493] hover:text-white rounded-2xl text-center" to="/projects">Projects</Link>
+                <Link
+                    className="py-1 px-2 border border-[#5b7493] text-[#5b7493] button-transition 
+                hover:bg-[#5b7493] hover:text-white rounded-2xl text-center"
+                    to="/projects">
+                    Projects
+                </Link>
             </div>
         </section>
     )
