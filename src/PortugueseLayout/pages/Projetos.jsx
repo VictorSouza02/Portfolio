ea3e613ac84b2713386299eb6d9143f404789591
Víctor Souza <102697200+VictorSouza02@users.noreply.github.com>
2023-05-15 19:06:22 -0300

    Projects Icon fixed

--- src/PortugueseLayout/pages/Projetos.jsx
+++ src/PortugueseLayout/pages/Projetos.jsx
@@ -7,10 +7,16 @@ const Projetos = () => {
             <div className='flex md:self-start items-center gap-2'>
                 <h1 className="text-4xl font-black">Projetos</h1>
 
-                <img className='hidden lg:block w-6 aspect-square cursor-help'
-                    data-bs-toggle="tooltip" title="Passe o mouse sobre um projeto para ver a descrição"
-                    src="https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/information-mark-circle-outline-icon.svg"
-                    alt="Info tooltip" />
+                <div
+                    className='hidden lg:block aspect-square cursor-help mt-2'
+                    data-bs-toggle="tooltip"
+                    title="Passe o mouse sobre um projeto para ver a descrição"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                    </svg>
+                </div>
             </div>
 
 
